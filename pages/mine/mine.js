487cd5229b25952ff5f744c055c478c8d77ac5e0
hoozle <plusltra@gmail.com>
2018-11-30 15:15:56 +0800

    提交文件

---
+++ pages/mine/mine.js
@@ -0,0 +1,12 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    isMe : true,
+    userUrl: "../resource/images/noneface.jpg",
+    videoSelClass: "video-info",
+    isSelectedWork: "video-info-selected"
+  },
+})
--- pages/register/register.js
+++ pages/register/register.js
@@ -6,5 +6,47 @@ Page({
   data: {
    
   },
+
+  doRegister: function(e){
+      var formObj = e.detail;
+      var username = formObj.username;
+      var password = formObj.password;
+      
+      if(0 == username.length || 0 == password.length)
+      {
+        wx.showToast({
+          title: '用户名或密码不能为空！',
+          duration: 3000
+        });        
+      }else{
+        var ServiceUrl = app.ServiceUrl;
+        wx.showLoading({
+          title: '请稍等！',
+        })
+        wx.request({
+          url: ServiceUrl + '/register',
+          method:POST,
+          dataType:JSON,
+          header:{
+            'content-type': 'application/json',
+          },
+          data:{
+            username: username,
+            password: password
+          },
+          success: function(res){
+
+          },
+
+        })
+      }
+
+  },
+
+  goLoginPage: function () {
+    wx.navigateTo({
+      url: '../login/login',
+    })
+  }
  
 })
